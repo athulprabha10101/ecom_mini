69d38a29566b17661e559ccbf4c77e3369c10d33
athulprabha10101 <alertsforathul@gmail.com>
2023-07-08 15:26:34 +0530

    working till amdin crud

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.admin_login, name="admin_login"),
     path('logout', views.admin_logout, name="admin_logout"),
     path('admin_home', views.admin_home, name="admin_home"),
+    path('block_user/<int:id>', views.block_user, name='block_user'),
+    path('test', views.test, name="test"),
 ]
--- customadmin/views.py
+++ customadmin/views.py
@@ -25,4 +25,20 @@ def admin_logout(request):
     return redirect('admin_login')
 
 def admin_home(request):
-    return render(request, 'customadmin/home.html')
+    if request.user.is_authenticated and request.user.is_superuser:
+        user_details = User.objects.filter(is_superuser=False)
+        return render(request, 'customadmin/homepage.html', {'user_details':user_details})    
+    return render(request, 'customadmin/login.html')
+
+def block_user(request, id):
+    user = User.objects.get(id=id)
+    if user.is_active:
+        user.is_active = False
+        user.save()
+    else:
+        user.is_active = True
+        user.save()
+    return redirect('admin_home')
+
+def test(request):
+    return render(request, 'customadmin/base.html')
