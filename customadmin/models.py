316e8b0b1a71207848f9f15ecb9a6ede76e9f7da
athulprabha10101 <alertsforathul@gmail.com>
2023-07-22 23:39:18 +0530

    Updated Schema - New tables and fields- migrations donegit add .

--- customadmin/models.py
+++ customadmin/models.py
@@ -6,18 +6,31 @@ class Category(models.Model):
     name = models.CharField(max_length=30, blank=False, null=False)
     image = models.ImageField(upload_to='category')
 
-class Products(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, null=False, blank=False)
-    quantity = models.IntegerField(null=False, blank=False)
-    original_price = models.FloatField(null=False, blank=False)
-    selling_price = models.FloatField(null=False, blank = False)
-    description = models.TextField(null=False, blank=False)
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    has_variant = models.BooleanField(default=False)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variations')
+    color = models.CharField(max_length=50, null=True, blank=True)
+    storage = models.CharField(max_length=50, null=True, blank=True)
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    color = models.ForeignKey(Variation, on_delete=models.CASCADE, null=True, blank=True, related_name='color_images')
+    image = models.ImageField(upload_to='product_images')
+
+class Variant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_variants')
+    color = models.ForeignKey(Variation, on_delete=models.CASCADE, null=True, blank=True, related_name='color_variants')
+    storage = models.ForeignKey(Variation, on_delete=models.CASCADE, null=True, blank=True, related_name='storage_variants')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    original_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
     is_deleted = models.BooleanField(default=False)
-    
-class ProductImage(models.Model):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='images')
-    product_image = models.ImageField(upload_to='product_images')
 
 class UserProfile(models.Model):
     name = models.CharField(max_length=100)
